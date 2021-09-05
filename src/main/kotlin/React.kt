d761d906c21545128a9fe9f3c4c4a11df63ee64f
Chris Laforet <Chris.Laforet@shuttereleasefilms.com>
2021-09-05 18:30:43 -0400

    First steps on solution

--- src/main/kotlin/React.kt
+++ src/main/kotlin/React.kt
@@ -4,4 +4,23 @@ class Reactor<T>() {
     interface Subscription {
         fun cancel()
     }
+
+    class Input<T>(public var value: T) {
+
+    }
+
+    class ValueOutput<T>(val value1: T, val value2: T) {
+        var value: T = value1
+    }
+
+    class CellOutput<T>(var input: Input<T>) {
+        var value: T = input.value
+    }
+
+    fun InputCell(value: T) : Input<T> = Input(value)
+
+    fun ComputeCell(value1: T, value2: T) : ValueOutput<T> = ValueOutput(value1, value2)
+
+    fun ComputeCell(input: Input<T>) : CellOutput<T> = CellOutput(input)
 }
+
